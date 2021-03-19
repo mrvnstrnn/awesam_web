c728b65f4ab2cee5cbd783548dcac3b3e87a66dd
Aris Salvador <arhizsx@gmail.com>
2021-03-19 15:22:01 +0800

    new commit

--- resources/views/profiles/vendor/index.blade.php
+++ resources/views/profiles/vendor/index.blade.php
@@ -2,49 +2,189 @@
 
 @section('content')
 
-{{-- <ul class="body-tabs body-tabs-layout tabs-animated body-tabs-animated nav">
-    <li class="nav-item">
-        <a role="tab" class="nav-link active"
-            id="tab-0" data-toggle="tab" href="#tab-content-0">
-            <span>Basic Calendar</span>
-        </a>
-    </li>
-    <li class="nav-item">
-        <a role="tab" class="nav-link" id="tab-1"
-            data-toggle="tab" href="#tab-content-1">
-            <span>List View</span>
-        </a>
-    </li>
-    <li class="nav-item">
-        <a role="tab" class="nav-link" id="tab-2"
-            data-toggle="tab" href="#tab-content-2">
-            <span>Background Events</span>
-        </a>
-    </li>
-</ul>
-<div class="tab-content">
-    <div class="tab-pane tabs-animation fade show active" id="tab-content-0" role="tabpanel">
-        <div class="main-card mb-3 card">
-            <div class="card-body">
-                <div id="calendar"></div>
+<div class="col-md-12 col-lg-6 col-xl-4">
+    <div class="main-card mb-3 card">
+        <div class="card-body">
+            <h6 class="text-muted text-uppercase font-size-md opacity-7 mb-3 font-weight-normal">
+                Highlights
+            </h6>
+            <div class="border-light card-border card">
+                <div class="scrollbar-container">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">
+                            <div class="widget-content p-0">
+                                <div class="widget-content-wrapper">
+                                    <div class="widget-content-left mr-3">
+                                        <img width="38" class="rounded-circle" src="images/avatars/1.jpg" alt="">
+                                    </div>
+                                    <div class="widget-content-left">
+                                        <div class="widget-heading">Beck Collier</div>
+                                        <div class="widget-subheading mt-1 opacity-10">
+                                            <div class="badge badge-pill badge-primary">$152</div>
+                                        </div>
+                                    </div>
+                                    <div class="widget-content-right">
+                                        <div class="fsize-2 text-success">
+                                            <small class="opacity-5 pr-1">$</small>
+                                            <span>750</span>
+                                            <small class="text-success pl-2">
+                                                <i class="fa fa-angle-down"></i>
+                                            </small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="widget-content p-0">
+                                <div class="widget-content-wrapper">
+                                    <div class="widget-content-left mr-3">
+                                        <img width="38" class="rounded-circle" src="images/avatars/2.jpg" alt="">
+                                    </div>
+                                    <div class="widget-content-left">
+                                        <div class="widget-heading">Angelo Hume</div>
+                                        <div class="widget-subheading mt-1 opacity-10">
+                                            <div class="badge badge-pill badge-primary">$53</div>
+                                        </div>
+                                    </div>
+                                    <div class="widget-content-right">
+                                        <div class="fsize-2 text-success">
+                                            <small class="opacity-5 pr-1">$</small>
+                                            <span>542</span>
+                                            <small class="text-danger pl-2">
+                                                <i class="fa fa-angle-up"></i>
+                                            </small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="widget-content p-0">
+                                <div class="widget-content-wrapper">
+                                    <div class="widget-content-left mr-3">
+                                        <img width="38" class="rounded-circle" src="images/avatars/2.jpg" alt="">
+                                    </div>
+                                    <div class="widget-content-left">
+                                        <div class="widget-heading">Saim Melendez</div>
+                                        <div class="widget-subheading mt-1 opacity-10">
+                                            <div class="badge badge-pill badge-primary">$239</div>
+                                        </div>
+                                    </div>
+                                    <div class="widget-content-right">
+                                        <div class="fsize-2 text-success">
+                                            <small class="opacity-5 pr-1">$</small>
+                                            <span>874</span>
+                                            <small class="text-warning pl-2">
+                                                <i class="fa fa-angle-down"></i>
+                                            </small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="widget-content p-0">
+                                <div class="widget-content-wrapper">
+                                    <div class="widget-content-left mr-3">
+                                        <img width="38" class="rounded-circle" src="images/avatars/3.jpg" alt="">
+                                    </div>
+                                    <div class="widget-content-left">
+                                        <div class="widget-heading">Primrose Navarro</div>
+                                        <div class="widget-subheading mt-1 opacity-10">
+                                            <div class="badge badge-pill badge-primary">$21</div>
+                                        </div>
+                                    </div>
+                                    <div class="widget-content-right">
+                                        <div class="fsize-2 text-success">
+                                            <small class="opacity-5 pr-1">$</small>
+                                            348
+                                            <small class="text-muted pl-2">
+                                                <i class="fa fa-angle-up"></i>
+                                            </small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="widget-content p-0">
+                                <div class="widget-content-wrapper">
+                                    <div class="widget-content-left mr-3">
+                                        <img width="38" class="rounded-circle" src="images/avatars/4.jpg" alt="">
+                                    </div>
+                                    <div class="widget-content-left">
+                                        <div class="widget-heading">Finnlay Barton</div>
+                                        <div class="widget-subheading mt-1 opacity-10">
+                                            <div class="badge badge-pill badge-primary">$381</div>
+                                        </div>
+                                    </div>
+                                    <div class="widget-content-right">
+                                        <div class="fsize-2 text-success">
+                                            <small class="opacity-5 pr-1">$</small>
+                                            692
+                                            <small class="text-muted pl-2">
+                                                <i class="fa fa-angle-up"></i>
+                                            </small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="widget-content p-0">
+                                <div class="widget-content-wrapper">
+                                    <div class="widget-content-left mr-3">
+                                        <img width="38" class="rounded-circle" src="images/avatars/10.jpg" alt="">
+                                    </div>
+                                    <div class="widget-content-left">
+                                        <div class="widget-heading">Johan Corbett</div>
+                                        <div class="widget-subheading mt-1 opacity-10">
+                                            <div class="badge badge-pill badge-primary">$74</div>
+                                        </div>
+                                    </div>
+                                    <div class="widget-content-right">
+                                        <div class="fsize-2 text-success">
+                                            <small class="opacity-5 pr-1">$</small>
+                                            395
+                                            <small class="text-muted pl-2">
+                                                <i class="fa fa-angle-down"></i>
+                                            </small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="border-bottom-0 list-group-item">
+                            <div class="widget-content p-0">
+                                <div class="widget-content-wrapper">
+                                    <div class="widget-content-left mr-3">
+                                        <img width="38" class="rounded-circle" src="images/avatars/9.jpg" alt="">
+                                    </div>
+                                    <div class="widget-content-left">
+                                        <div class="widget-heading">Kaja Wolfe</div>
+                                        <div class="widget-subheading mt-1 opacity-10">
+                                            <div class="badge badge-pill badge-primary">$7</div>
+                                        </div>
+                                    </div>
+                                    <div class="widget-content-right">
+                                        <div class="fsize-2 text-success">
+                                            <small class="opacity-5 pr-1">$</small>
+                                            619
+                                            <small class="text-muted pl-2">
+                                                <i class="fa fa-angle-down"></i>
+                                            </small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
-    <div class="tab-pane tabs-animation fade" id="tab-content-1" role="tabpanel">
-        <div class="main-card mb-3 card">
-            <div class="card-body">
-                <div id="calendar-list"></div>
-            </div>
-        </div>
-    </div>
-    <div class="tab-pane tabs-animation fade" id="tab-content-2" role="tabpanel">
-        <div class="main-card mb-3 card">
-            <div class="card-body">
-                <div id="calendar-bg-events"></div>
-            </div>
-        </div>
-    </div>
-</div> --}}
+</div>
 
 
 @endsection
