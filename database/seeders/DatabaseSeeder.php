d1f0f0b748f66ed0ccd6dc4f976466a4708f976e
Marvin Esternon <maesternon@absi.ph>
2021-04-28 14:09:24 +0800

    Add user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,9 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
         $this->call(RoleSeeder::class);
         $this->call(PermissionSeeder::class);
         $this->call(RolePermissionSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
