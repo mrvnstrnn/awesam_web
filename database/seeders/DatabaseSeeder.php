06cf3c346734a2c6789d7c625b5a5419d1e5b554
Marvin Esternon <maesternon@absi.ph>
2021-05-02 04:37:24 +0800

    features0037-new-slugs-new-faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\Company::factory(10)->create();
-        \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(10)->create();
         $this->call(ProfileSeeder::class);
         $this->call(PermissionSeeder::class);
         $this->call(ProfilePermissionSeeder::class);
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\Profile;
 use Illuminate\Support\Facades\Hash;
 use Carbon\Carbon;
 
@@ -16,14 +17,19 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
-            'firstname' => 'Globe',
-            'lastname' => 'Sam',
-            'name' => 'Globe Sam',
-            'email' => 'sam@globe.ph',
-            'profile_id' => 7,
-            'email_verified_at' => Carbon::now()->toDate(),
-            'password' => Hash::make('12345678'),
-        ]);
+        $profiles = Profile::get();
+
+        foreach ($profiles as $profile) {
+            $name = explode(' ', trim($profile->profile));
+            User::create([
+                'firstname' => $name[0],
+                'lastname' => count($name) < 2 ? ucfirst($profile->mode) : $name[1],
+                'name' => count($name) < 2 ? ucfirst($profile->mode) : $name[0]. ' ' .$name[1],
+                'email' => strtolower(str_replace(' ', '-', $profile->profile)).'@'.$profile->mode.'.ph',
+                'profile_id' => $profile->id,
+                'email_verified_at' => Carbon::now()->toDate(),
+                'password' => Hash::make('12345678'),
+            ]);
+        }
     }
 }
