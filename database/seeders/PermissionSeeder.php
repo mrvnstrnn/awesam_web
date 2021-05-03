75c30d3dd11aff1fe70eba44e395cb2d201b4f9e
Marvin Esternon <maesternon@absi.ph>
2021-05-04 03:25:12 +0800

    add blade file and permissions for agent profile

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -42,12 +42,14 @@ class PermissionSeeder extends Seeder
             ['New Endorsement', 'new-endorsement', 'New Endorsement', 'Newly Endorsed Sites', 'profile_menu', 'new endorsement', '', 'note2'],
             ['Universe', 'universe', 'Universe', 'Universe List', 'profile_menu', 'sites', '', 'global'],
             ['Requests', 'requests', 'Requests', 'Schedule Change Request', 'profile_menu', 'schedule', '', 'network'],
-            ['Actvities', 'activities', 'Actvities', 'Agent Activities', 'profile_menu', 'activities', '', 'graph1'],
+            ['Actvities', 'activities', 'Actvities', 'Agent Activities', 'profile_menu', 'tasks', '', 'graph1'],
             ['Agents', 'agents', 'Agents', 'Assigned Agents To Sup', 'profile_menu', 'agents', '', 'network'],
             // ['Notifications', 'notifications', 'Notifications', 'Notifications', 'profile_menu', 'notifications', '', 'bell'],
             ['Supervisor', 'supervisor', 'Supervisor', 'Supervisor List', 'profile_menu', 'supervisor', '', 'users'],
             ['Vendor', 'vendor', 'Vendor', 'Vendor List', 'profile_menu', 'vendor', '', 'users'],
             ['Approvals', 'approvals', 'Approvals', 'Approvals List', 'profile_menu', 'sites', '', 'global'],
+            ['New Vendor', 'newvendor', 'New Vendor', 'New Vendor', 'profile_menu', 'vendor', '', 'users'],
+            ['Assigned Sites', 'assigned-sites', 'Assigned Sites', 'Assigned Sites', 'profile_menu', 'tasks', '', 'graph1'],
         ];
 
         for ($i=0; $i < count($data); $i++) { 
