2a540cc6bc4d11443b302751e93176e3e17334c2
Aris Salvador <zfdsalvador@globe.com.ph>
2021-05-11 11:59:20 +0800

    reformatted add vendor form

--- resources/views/profiles/globe/sts-vendor-admin/newvendor.blade.php
+++ resources/views/profiles/globe/sts-vendor-admin/newvendor.blade.php
@@ -1,118 +1,137 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="main-card mb-3 card">
-    <div class="card-body">
-        <div class="row">
-            <div class="col-md-12">
-                <h5 class="card-title">Add new vendor</h5>
+        <div class="col-md-12">
+            <div class="main-card mb-3 card">
+                <div class="card-header-tab card-header">
+                    <div class="card-header-title font-size-lg text-capitalize font-weight-normal">
+                    <i class="header-icon lnr-apartment icon-gradient bg-ripe-malin"></i>
+                    Add New Vendor
+                    </div>
+                </div>
                 <form id="addVendorForm">
-                    @csrf
-                    <input type="hidden" name="vendor_id" id="vendor_id">
-                    <div class="form-row">
-                        <div class="col-md-6">
-                            <div class="position-relative form-group">
-                                <label for="vendor_sec_reg_name">SEC Registered Vendor Name</label>
-                                <input id="vendor_sec_reg_name" type="text" class="form-control" 
-                                    name="vendor_sec_reg_name" required autofocus placeholder="Registered vendor name here...">
-            
-                                <small id="vendor_sec_reg_name-error" class="form-text text-danger"></small>
-                            </div>
-                        </div>
-        
-                        <div class="col-md-6">
-                            <div class="position-relative form-group">
-                                <label for="vendor_acronym">Acronym Vendor Name </label>
-                                <input id="vendor_acronym" type="text" class="form-control" 
-                                    name="vendor_acronym" required placeholder="Vendor acronym name here...">
-            
-                                <small id="vendor_acronym-error" class="form-text text-danger"></small>
-                            </div>
-                        </div>
-                    </div>
-        
-                    <div class="form-row">
-                        <div class="col-md-6">
-                            <div class="position-relative form-group">
-                                <label for="vendor_fullname">Vendor System Admin Fullname </label>
-                                <input id="vendor_fullname" type="text" class="form-control" 
-                                    name="vendor_fullname" required placeholder="Vendor system admin fullname here...">
-            
-                                <small id="vendor_fullname-error" class="form-text text-danger"></small>
-                            </div>
-                        </div>
-        
-                        <div class="col-md-6">
-                            <div class="position-relative form-group">
-                                <label for="vendor_admin_email">Vendor System Admin Email Address</label>
-                                <input id="vendor_admin_email" type="email" class="form-control" 
-                                    name="vendor_admin_email" required placeholder="Vendor system admin email address here...">
-            
-                                <small id="vendor_admin_email-error" class="form-text text-danger"></small>
-                            </div>
-                        </div>
-                    </div>
-        
-                    <div class="form-row">
-                        <div class="col-12">
-                            <div class="position-relative form-group">
-                                <label for="vendor_office_address">Official Head Office Address</label>
-                                <input id="vendor_office_address" type="text" class="form-control" 
-                                    name="vendor_office_address" required placeholder="Official head office address here...">
-            
-                                <small id="vendor_office_address-error" class="form-text text-danger"></small>
-                            </div>
-                        </div>
-                    </div>
-        
-                    <div class="form-row">
-                        <div class="col-md-6">
-                            <div class="position-relative form-group">
-                                <label for="vendor_saq_status">Status of SAQ </label>
-                                <select name="vendor_saq_status" id="vendor_saq_status" class="form-control" required>
-                                    <option value="">Please select status</option>
-                                    <option value="1">Active</option>
-                                    <option value="0">Ongoing Accreditation</option>
-                                </select>
-            
-                                <small id="vendor_saq_status-error" class="form-text text-danger"></small>
-                            </div>
-                        </div>
-        
-                        <div class="col-md-6">
-                            <div class="position-relative form-group">
-                                <label for="vendor_admin_email">Program</label><br>
-        
-                                @php
-                                    $programs = App\Models\VendorProgram::get();
-                                @endphp
-        
-                                @foreach ($programs as $program)
-                                    <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="checkbox" id="vendor_program_id{{ $program->vendor_program_id  }}" name="vendor_program_id" value="{{ $program->vendor_program_id  }}">
-                                        <label class="form-check-label" for="vendor_program_id{{ $program->vendor_program_id  }}">{{ $program->vendor_program }}</label>
+                    <div class="card-body">
+                            @csrf
+                            <input type="hidden" name="vendor_id" id="vendor_id">
+                            <div class="form-row">
+                                <div class="col-md-3">
+                                    <H5>Details</H5>
+                                </div>                
+                                <div class="col-md-9">
+                                    <div class="position-relative form-group">
+                                        <label for="vendor_sec_reg_name">SEC Registered Vendor Name</label>
+                                        <input id="vendor_sec_reg_name" type="text" class="form-control" 
+                                            name="vendor_sec_reg_name" required autofocus placeholder="Registered vendor name here...">
+                    
+                                        <small id="vendor_sec_reg_name-error" class="form-text text-danger"></small>
                                     </div>
-                                @endforeach
-            
-                                <small id="vendor_program_id-error" class="form-text text-danger"></small>
+                                    <div class="position-relative form-group">
+                                        <label for="vendor_acronym">Acronym Vendor Name </label>
+                                        <input id="vendor_acronym" type="text" class="form-control" 
+                                            name="vendor_acronym" required placeholder="Vendor acronym name here...">
+                    
+                                        <small id="vendor_acronym-error" class="form-text text-danger"></small>
+                                    </div>
+                                    <div class="position-relative form-group">
+                                        <label for="vendor_office_address">Official Head Office Address</label>
+                                        <input id="vendor_office_address" type="text" class="form-control" 
+                                            name="vendor_office_address" required placeholder="Official head office address here...">
+                    
+                                        <small id="vendor_office_address-error" class="form-text text-danger"></small>
+                                    </div>
+                                </div>
+                            </div>          
+                            <div class="divider"></div>                            
+                            <div class="form-row">
+                                <div class="col-md-3">
+                                    <H5>Status</H5>
+                                </div>
+                                <div class="col-md-9">
+                                    <div class="position-relative form-group">
+                                    {{-- <fieldset class="position-relative row form-group"> --}}
+                                    {{-- <label for="vendor_saq_status">Status of Vendor </label> --}}
+                                        <div class="position-relative form-check-inline">
+                                            <label class="form-check-label">
+                                                <input name="vendor_saq_status" id="vendor_saq_status" type="radio" class="form-check-input">
+                                                Active
+                                            </label>
+                                        </div>
+                                        <div class="position-relative form-check-inline">
+                                            <label class="form-check-label">
+                                                <input name="vendor_saq_status" id="vendor_saq_status" type="radio" class="form-check-input">
+                                                Ongoing Accreditation
+                                            </label>
+                                        </div>
+                                        <small id="vendor_saq_status-error" class="form-text text-danger"></small>
+                                    {{-- </fieldset>                                         --}}
+
+
+                                        {{-- <label for="vendor_saq_status">Status of Vendor </label>
+                                        <select name="vendor_saq_status" id="vendor_saq_status" class="form-control" required>
+                                            <option value="">Please select status</option>
+                                            <option value="1">Active</option>
+                                            <option value="0">Ongoing Accreditation</option>
+                                        </select> --}}
+                    
+                                    </div>
+                                </div>
                             </div>
-                        </div>
+                            <div class="divider"></div>                            
+                            <div class="form-row">
+                                    <div class="col-md-3">
+                                        <H5>Programs</H5>
+                                    </div>
+                                    <div class="col-md-9">
+                                        <div class="position-relative form-group">
+                                            {{-- <label for="vendor_admin_email">Program</label><br> --}}
+                    
+                                            @php
+                                                $programs = App\Models\VendorProgram::get();
+                                            @endphp
+                    
+                                            @foreach ($programs as $program)
+                                                <div class="form-check form-check-inline">
+                                                    <input class="form-check-input" type="checkbox" id="vendor_program_id{{ $program->vendor_program_id  }}" name="vendor_program_id" value="{{ $program->vendor_program_id  }}">
+                                                    <label class="form-check-label" for="vendor_program_id{{ $program->vendor_program_id  }}">{{ $program->vendor_program }}</label>
+                                                </div>
+                                            @endforeach
+                        
+                                            <small id="vendor_program_id-error" class="form-text text-danger"></small>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="divider"></div>                            
+                            <div class="form-row">
+                                <div class="col-md-3">
+                                    <H5>Administrator</H5>
+                                </div>
+                                <div class="col-md-9">
+                                    <div class="position-relative form-group">
+                                        <label for="vendor_fullname">Admin Fullname </label>
+                                        <input id="vendor_fullname" type="text" class="form-control" 
+                                            name="vendor_fullname" required placeholder="Vendor system admin fullname here...">
+                    
+                                        <small id="vendor_fullname-error" class="form-text text-danger"></small>
+                                    </div>
+                                    <div class="position-relative form-group">
+                                        <label for="vendor_admin_email">Admin Email Address</label>
+                                        <input id="vendor_admin_email" type="email" class="form-control" 
+                                            name="vendor_admin_email" required placeholder="Vendor system admin email address here...">
+                    
+                                        <small id="vendor_admin_email-error" class="form-text text-danger"></small>
+                                    </div>
+                                </div>
+                            </div>
                     </div>
-                    <div class="card-footer">
-                        <a href="javascript:void(0)" class="btn-shadow btn-wide mr-2 float-right btn-pill btn-hover-shine btn btn-default resetForm">Reset</a>
-                        <button class="btn btn-primary pull-right add_vendor" type="button" data-href="{{ route('add.vendor') }}">Add vendor</button>
+                    <div class="d-block text-right card-footer">
+                        <a href="javascript:void(0)" class="btn-shadow btn-wide mr-2 btn-pill btn-hover-shine btn btn-default resetForm">Reset</a>
+                        <button class="btn btn-primary add_vendor" type="button" data-href="{{ route('add.vendor') }}">Add vendor</button>
+                        {{-- <button type="button" class="btn btn btn-outline-danger btn-bulk-acceptreject-endorsement" data-program="coloc" data-complete="false" id="" data-href="{{ route('accept-reject.endorsement') }}">Reject</button>
+                        <button type="button" class="btn btn-primary btn-bulk-acceptreject-endorsement" data-program="coloc" data-complete="true" id="" data-href="{{ route('accept-reject.endorsement') }}">Accept Endorsement</button> --}}
                     </div>
                 </form>
             </div>
-            
-            {{-- <div class="col-md-2">
-                <h5 class="card-title">Review Existing Vendors</h5>
-                <a href="javascript:void(0);" class="list_vendors">Click Here & Show List</a>
-            </div> --}}
         </div>
-    </div>
-</div>
-
 @endsection
 
 @section('modals')
