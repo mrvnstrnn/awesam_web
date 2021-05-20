a1403c936cadbf0e519f2ad6a130fc19e3dbd634
Aris Salvador <zfdsalvador@globe.com.ph>
2021-05-20 20:13:11 +0800

    Added Agent Site Status Card

--- resources/views/profiles/vendor/agent/assigned-sites.blade.php
+++ resources/views/profiles/vendor/agent/assigned-sites.blade.php
@@ -73,6 +73,7 @@
                                 </div>
                             </div>
                             <div class="card-body">
+                                
 
                             </div>
                         </div>
