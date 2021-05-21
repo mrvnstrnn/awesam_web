4edffd2de7160b6ab83964e376cfdf1f34ba493d
Aris Salvador <zfdsalvador@globe.com.ph>
2021-05-21 15:30:32 +0800

    card label mods

--- resources/views/profiles/vendor/supervisor/agents.blade.php
+++ resources/views/profiles/vendor/supervisor/agents.blade.php
@@ -46,7 +46,7 @@
                             <div class="card-header-tab card-header">
                                 <div class="card-header-title font-size-lg text-capitalize font-weight-normal">
                                 <i class="header-icon lnr-layers icon-gradient bg-ripe-malin"></i>
-                                {{ strtoupper($program->program)  }} Endorsements
+                                {{ strtoupper($program->program)  }} Agents
                                 </div>      
                             </div>
                             <div class="card-body">
