584e9c664ad313cb3feedd2a06dfe2014db75e08
Aris Salvador <zfdsalvador@globe.com.ph>
2021-06-02 16:12:50 +0800

    Sub Activity Drag and Drop Upload

--- resources/views/profiles/vendor/agent/activities.blade.php
+++ resources/views/profiles/vendor/agent/activities.blade.php
@@ -27,7 +27,20 @@
         
     }
 
+    .dropzone {
+        margin-top:0px;
+        width: "100%";
+        /* max-width: "100%"; */
+        margin-left: auto;
+        margin-right: auto;
+        margin: 0px 0 0 0;
+        background: #e3e6ff;
+        border: 1px dashed blue;
+    }
+
+
 </style>
+
 
 <ul class="tabs-animated body-tabs-animated nav">
     <li class="nav-item">
@@ -223,12 +236,19 @@
                                                                 @endphp
                                                             </div>
                                                             <div class="row action_box d-none">
-                                                                <div class="position-relative form-group pl-3">
-                                                                    <label for="exampleFile" class="">File</label>
-                                                                    <input name="file" id="exampleFile" type="file" class="form-control-file">
-                                                                    <small class="form-text text-muted">
-                                                                    </small>
-                                                                </div>                                                        
+                                                                
+
+
+                                                                    <form action="/drop" style="margin-top:0px;" class="col-md-12 p-0 dropzone" id="uploadFile_{{ $activities_groups[array_keys($activities_groups)[$j]][$k]->sam_id }}_{{ $activities_groups[array_keys($activities_groups)[$j]][$k]->activity_id }}" enctype="multipart/form-data">
+                                                                        @csrf
+                                                                        <div class="dz-message">
+                                                                            Drag 'n' Drop Files<br>
+                                                                        </div>
+                                                                    </div>
+                                                                    
+                                                                    </form>
+                                                                
+                                                                
                                                             </div>
                                                         </div>
                                                     </li>
@@ -795,7 +815,14 @@
 <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
 
-    <script>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.7.2/dropzone.min.css" rel="stylesheet">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.7.2/min/dropzone.min.js"></script>
+
+
+
+<script>
+    Dropzone.autoDiscover = false;
+
 $(document).ready(function() {
 
 
@@ -829,6 +856,67 @@ $(document).ready(function() {
 
             $(where + " .lister").toggleClass("d-none");
             $(where + " .action_box").toggleClass("d-none");
+
+            var dropzone = new Dropzone('#uploadFile_' + $(this).attr('data-sam_id') + "_" + $(this).attr('data-activity_id'), {
+            // previewTemplate: document.querySelector('#preview-template').innerHTML,
+            parallelUploads: 3,
+            thumbnailHeight: 180,
+            thumbnailWidth: 180,
+            maxFilesize: 6,
+            filesizeBase: 1500,
+            thumbnail: function (file, dataUrl) {
+                if (file.previewElement) {
+                    file.previewElement.classList.remove("dz-file-preview");
+                    var images = file.previewElement.querySelectorAll("[data-dz-thumbnail]");
+                    for (var i = 0; i < images.length; i++) {
+                        var thumbnailElement = images[i];
+                        thumbnailElement.alt = file.name;
+                        thumbnailElement.src = dataUrl;
+                    }
+                    setTimeout(function () {
+                        file.previewElement.classList.add("dz-image-preview");
+                    }, 1);
+                }
+            }
+        });
+        
+        var minSteps = 6,
+            maxSteps = 60,
+            timeBetweenSteps = 100,
+            bytesPerStep = 100000;
+
+        dropzone.uploadFiles = function (files) {
+            var self = this;
+
+            for (var i = 0; i < files.length; i++) {
+
+                var file = files[i];
+                totalSteps = Math.round(Math.min(maxSteps, Math.max(minSteps, file.size / bytesPerStep)));
+
+                for (var step = 0; step < totalSteps; step++) {
+                    var duration = timeBetweenSteps * (step + 1);
+                    setTimeout(function (file, totalSteps, step) {
+                        return function () {
+                            file.upload = {
+                                progress: 100 * (step + 1) / totalSteps,
+                                total: file.size,
+                                bytesSent: (step + 1) * file.size / totalSteps
+                            };
+
+                            self.emit('uploadprogress', file, file.upload.progress, file.upload
+                                .bytesSent);
+                            if (file.upload.progress == 100) {
+                                file.status = Dropzone.SUCCESS;
+                                self.emit("success", file, 'success', null);
+                                self.emit("complete", file);
+                                self.processQueue();
+                            }
+                        };
+                    }(file, totalSteps, step), duration);
+                }
+            }
+        }    
+
             
             // console.log( $("#sub_activity_COLOC-000007").attr("class") );
             
@@ -842,6 +930,7 @@ $(document).ready(function() {
         }
 
     });
+
 
 
 
