47f6f918662dbaefa08c0bd9350e6566d4da8f06
Marvin Esternon <maesternon@absi.ph>
2021-06-05 20:32:26 +0800

    update

--- app/Mail/GTInvitationMail.php
+++ app/Mail/GTInvitationMail.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class GTInvitationMail extends Mailable implements ShouldQueue
+class GTInvitationMail extends Mailable
 {
     use Queueable, SerializesModels;
 
--- app/Mail/InvitationMail.php
+++ app/Mail/InvitationMail.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class InvitationMail extends Mailable implements ShouldQueue
+class InvitationMail extends Mailable
 {
     use Queueable, SerializesModels;
 
