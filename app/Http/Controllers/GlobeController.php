f6f9dc7e9eb7f52b03e04b8d7131f4a2d11b952f
Marvin Esternon <maesternon@absi.ph>
2021-06-09 09:17:05 +0800

    update

--- app/Http/Controllers/GlobeController.php
+++ app/Http/Controllers/GlobeController.php
@@ -455,6 +455,40 @@ class GlobeController extends Controller
         }
     }
 
+    public function fileupload(Request $request){
+
+        try {
+
+            return response()->json(['error' => true, 'message' => $request->all()]);
+            if($request->hasFile('file')) {
+   
+                // Upload path
+                $destinationPath = 'files/';
+            
+                // Get file extension
+                $extension = $request->file('file')->getClientOriginalExtension();
+            
+                // Valid extensions
+                $validextensions = array("jpeg","jpg","png","pdf");
+            
+                // Check extension
+                if(in_array(strtolower($extension), $validextensions)){
+            
+                    // Rename file 
+                    $fileName = $request->file('file')->getClientOriginalName().time() .'.' . $extension;
+                    // Uploading file to given path
+                    $request->file('file')->move($destinationPath, $fileName); 
+            
+                }
+        
+            }
+
+            return response()->json(['error' => false, 'message' => "Successfully printed to pdf."]);
+        } catch (\Throwable $th) {
+            return response()->json(['error' => true, 'message' => $th->getMessage()]);
+        }
+    }
+
 
 }
 
--- resources/views/layouts/header/grid_dashboard.blade.php
+++ resources/views/layouts/header/grid_dashboard.blade.php
@@ -4,77 +4,45 @@ class="dropdown-menu-xl rm-pointers dropdown-menu dropdown-menu-right">
     <div class="dropdown-menu-header-inner bg-plum-plate">
         <div class="menu-header-image" style="background-image: url('images/dropdown-header/abstract4.jpg');"></div>
         <div class="menu-header-content text-white">
-            <h5 class="menu-header-title">Profile Switcher</h5>
-            <h6 class="menu-header-subtitle">superuser easy profile switching</h6>
+            <h5 class="menu-header-title">Agent Tools</h5>
+            {{-- <h6 class="menu-header-subtitle"></h6> --}}
         </div>
     </div>
 </div>
 <div class="grid-menu grid-menu-xl grid-menu-3col">
     <div class="no-gutters row">
-        <?php $roles = \Auth::user()->allRoles() ?>
+        {{-- <?php $roles = \Auth::user()->allRoles() ?> --}}
 
-        @foreach ($roles as $role)
+        {{-- @foreach ($roles as $role)
             <div class="col-sm-6 col-xl-4">
                 <a href='{{ route('profile.switcher', $role->id) }}' class="btn-icon-vertical btn-square btn-transition btn btn-outline-link {{ \Auth::user()->profile_id == $role->id ? 'disabled' : '' }}">
                     <i class="pe-7s-{{ $role->mode == 'vendor' ? 'user' : 'global' }} icon-gradient bg-night-fade btn-icon-wrapper btn-icon-lg mb-3"></i>
                     {{ ucfirst($role->mode) . ' : ' . $role->profile }}
                 </a>
             </div>
-        @endforeach
-        {{-- <div class="col-sm-6 col-xl-4">
-            <a href='/profile-switcher/vendor/head' class="btn-icon-vertical btn-square btn-transition btn btn-outline-link">
-                <i class="pe-7s-user icon-gradient bg-night-fade btn-icon-wrapper btn-icon-lg mb-3"></i>
-                Vendor : Head
+        @endforeach --}}
+
+        <div class="col-sm-6 col-xl-4">
+            <a href='#' class="btn-icon-vertical btn-square btn-transition btn btn-outline-link">
+                <i class="pe-7s-calculator icon-gradient bg-night-fade btn-icon-wrapper btn-icon-lg mb-3"></i>
+                TCO LEASE CALCULATOR
             </a>
         </div>
+
         <div class="col-sm-6 col-xl-4">
-            <a href='/profile-switcher/vendor/manager' class="btn-icon-vertical btn-square btn-transition btn btn-outline-link">
-                <i class="pe-7s-user icon-gradient bg-night-fade btn-icon-wrapper btn-icon-lg mb-3"></i>
-                Vendor : Manager
+            <a href='#' class="btn-icon-vertical btn-square btn-transition btn btn-outline-link">
+                <i class="pe-7s-calculator icon-gradient bg-night-fade btn-icon-wrapper btn-icon-lg mb-3"></i>
+                X-DEAL CALCULATOR
             </a>
         </div>
+
         <div class="col-sm-6 col-xl-4">
-            <a href='/profile-switcher/vendor/supervisor' class="btn-icon-vertical btn-square btn-transition btn btn-outline-link">
-                <i class="pe-7s-user icon-gradient bg-night-fade btn-icon-wrapper btn-icon-lg mb-3"></i>
-                Vendor : Supervisor
+            <a href='#' class="btn-icon-vertical btn-square btn-transition btn btn-outline-link">
+                <i class="pe-7s-calculator icon-gradient bg-night-fade btn-icon-wrapper btn-icon-lg mb-3"></i>
+                FILE BIN
             </a>
         </div>
-        <div class="col-sm-6 col-xl-4">
-            <a href='/profile-switcher/vendor/agent' class="btn-icon-vertical btn-square btn-transition btn btn-outline-link">
-                <i class="pe-7s-user icon-gradient bg-night-fade btn-icon-wrapper btn-icon-lg mb-3"></i>
-                Vendor : Agent
-            </a>
-        </div>
-        <div class="col-sm-6 col-xl-4">
-            <a href='/profile-switcher/globe/sts_head' class="btn-icon-vertical btn-square btn-transition btn btn-outline-link">
-                <i class="pe-7s-global icon-gradient bg-night-fade btn-icon-wrapper btn-icon-lg mb-3"></i>
-                Globe : STS Head
-            </a>
-        </div>
-        <div class="col-sm-6 col-xl-4">
-            <a href='/profile-switcher/globe/sts_staff' class="btn-icon-vertical btn-square btn-transition btn btn-outline-link">
-                <i class="pe-7s-global icon-gradient bg-night-fade btn-icon-wrapper btn-icon-lg mb-3"></i>
-                Globe : STS Staff
-            </a>
-        </div>
-        <div class="col-sm-6 col-xl-4">
-            <a href='/profile-switcher/globe/program_head' class="btn-icon-vertical btn-square btn-transition btn btn-outline-link">
-                <i class="pe-7s-global icon-gradient bg-night-fade btn-icon-wrapper btn-icon-lg mb-3"></i>
-                Globe : Program Head
-            </a>
-        </div>
-        <div class="col-sm-6 col-xl-4">
-            <a href='/profile-switcher/globe/pprogram_supervisor' class="btn-icon-vertical btn-square btn-transition btn btn-outline-link">
-                <i class="pe-7s-global icon-gradient bg-night-fade btn-icon-wrapper btn-icon-lg mb-3"></i>
-                Globe : Program Supervisor
-            </a>
-        </div>
-        <div class="col-sm-6 col-xl-4">
-            <a href='/profile-switcher/globe/program_staff' class="btn-icon-vertical btn-square btn-transition btn btn-outline-link">
-                <i class="pe-7s-global icon-gradient bg-night-fade btn-icon-wrapper btn-icon-lg mb-3"></i>
-                Globe : Program Staff
-            </a>
-        </div> --}}
+
     </div>
 </div>
 </div>
