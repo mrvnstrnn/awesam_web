fd0fb78a57578e91db9ebe6c5dd9a3ac2532d3d8
Aris Salvador <zfdsalvador@globe.com.ph>
2021-06-09 14:48:51 +0800

    Site Chat

--- resources/views/profiles/vendor/supervisor/view_site.blade.php
+++ resources/views/profiles/vendor/supervisor/view_site.blade.php
@@ -49,6 +49,12 @@
                     <span>Issues</span>
                 </a>
             </li>
+            <li class="nav-item">
+                <a role="tab" class="nav-link" id="tab-5" data-toggle="tab" href="#tab-content-5">
+                    <span>Site Chat</span>
+                </a>
+            </li>
+
         </ul>
 
         <div class="tab-content">
@@ -318,103 +324,15 @@
                 </div>
             </div>
 
-        </div>
-    </div>
-
-    <div class="col-md-4">
-        <ul class="tabs-animated body-tabs-animated nav">
-            <li class="nav-item">
-                <a role="tab" class="nav-link active" id="tab2-1" data-toggle="tab" href="#tab2-content-1">
-                    <span>Agent</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a role="tab" class="nav-link" id="tab2-0" data-toggle="tab" href="#tab2-content-0">
-                    <span>Chat</span>
-                </a>
-            </li>
-        </ul>
-
-        <div class="tab-content">
-            <div class="tab-pane tabs-animation fade active show" id="tab2-content-1" role="tabpanel">
-                <div class="mb-3 profile-responsive card">
-                    <div class="dropdown-menu-header">
-                        <div class="dropdown-menu-header-inner bg-dark">
-                            <div class="menu-header-image opacity-2" style="background-image: url('/images/dropdown-header/abstract2.jpg');"></div>
-                            <div class="menu-header-content btn-pane-right">
-                                <div class="avatar-icon-wrapper mr-3 avatar-icon-xl btn-hover-shine">
-                                    <div class="avatar-icon rounded">
-                                        <img src="/images/avatars/3.jpg" alt="Avatar 5">
-                                    </div>
-                                </div>
-                                <div>
-                                    <h5 class="menu-header-title">{{ $agent_name }}</h5>
-                                    <h6 class="menu-header-subtitle">Agent</h6>
-                                </div>
-                            </div>
-                        </div>
-                    </div>                    
-                    <ul class="list-group list-group-flush">
-                        @foreach($agent_sites as $what_site)
-                        <li class="list-group-item">
-                            <div class="widget-content p-0">
-                                <div class="widget-content-wrapper">
-                                    <div class="widget-content-left mr-3">
-                                        <div class="icon-wrapper m-0">
-                                            <div class="progress-circle-wrapper">
-                                                <div class="circle-progress d-inline-block circle-progress-success-sm">
-                                                    <small><span>81%<span></span></span></small>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="widget-content-left">
-                                        <div class="widget-heading"><a href="{{ route('view_assigned_site',[$what_site->sam_id]) }}">{{ $what_site->site_name }}</a></div>
-                                        <div class="widget-subheading">{{ $what_site->sam_id }}</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>                        
-                        @endforeach
-                    </ul>
-                </div>
-            </div>
-            <div class="tab-pane tabs-animation fade" id="tab2-content-0" role="tabpanel">
-                <div class="card-hover-shadow-2x mb-3 card">
+            <div class="tab-pane tabs-animation fade" id="tab-content-5" role="tabpanel">
+                <div class="main-card mb-3 card">
                     <div class="card-header-tab card-header">
                         <div class="card-header-title font-size-lg text-capitalize font-weight-normal">
-                            <i class="header-icon lnr-bubble icon-gradient bg-ripe-malin"></i>
-                            Chat Box
-                        </div>
-                        <div class="btn-actions-pane-right text-capitalize actions-icon-btn">
-                            <div class="btn-group dropdown">
-                                <button type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="btn-icon btn-icon-only btn btn-link">
-                                    <i class="pe-7s-menu btn-icon-wrapper"></i>
-                                </button>
-                                <div tabindex="-1" role="menu" aria-hidden="true" class="dropdown-menu-right rm-pointers dropdown-menu-shadow dropdown-menu-hover-link dropdown-menu">
-                                    <h6 tabindex="-1" class="dropdown-header">Header</h6>
-                                    <button type="button" tabindex="0" class="dropdown-item">
-                                        <i class="dropdown-icon lnr-inbox"></i>
-                                        <span>Menus</span>
-                                    </button>
-                                    <button type="button" tabindex="0" class="dropdown-item">
-                                        <i class="dropdown-icon lnr-file-empty"></i>
-                                        <span>Settings</span>
-                                    </button>
-                                    <button type="button" tabindex="0" class="dropdown-item">
-                                        <i class="dropdown-icon lnr-book"></i>
-                                        <span>Actions</span>
-                                    </button>
-                                    <div tabindex="-1" class="dropdown-divider"></div>
-                                    <div class="p-3 text-right">
-                                        <button class="mr-2 btn-shadow btn-sm btn btn-link">View Details</button>
-                                        <button class="mr-2 btn-shadow btn-sm btn btn-primary">Action</button>
-                                    </div>
-                                </div>
-                            </div>
+                            <i class="header-icon lnr-question-circle icon-gradient bg-ripe-malin"></i>
+                            {{ $title }}
                         </div>
                     </div>
-                    <div class="scroll-area-lg">
+                    <div class="card-body">
                         <div class="scrollbar-container ps ps--active-y">
                             <div class="p-2">
                                 <div class="chat-wrapper p-1">
@@ -526,13 +444,60 @@
                                 </div>
                             </div>
                         <div class="ps__rail-x" style="left: 0px; bottom: 0px;"><div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps__rail-y" style="top: 0px; height: 400px; right: 0px;"><div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 265px;"></div></div></div>
+
                     </div>
-                    <div class="card-footer">
-                        <input placeholder="Write here and hit enter to send..." type="text" class="form-control-sm form-control">
+                    <div class="d-block text-right card-footer">
+                        <button class="mr-2 btn btn-link btn-sm">Cancel</button>
+                        <button class="btn btn-success btn-lg">Save</button>
                     </div>
-                </div>                
+                </div>
             </div>
+
         </div>
+    </div>
+
+    <div class="col-md-4">
+            <div class="mb-3 profile-responsive card" style="margin-top: 80px;">
+                <div class="dropdown-menu-header">
+                    <div class="dropdown-menu-header-inner bg-dark">
+                        <div class="menu-header-image opacity-2" style="background-image: url('/images/dropdown-header/abstract2.jpg');"></div>
+                        <div class="menu-header-content btn-pane-right">
+                            <div class="avatar-icon-wrapper mr-3 avatar-icon-xl btn-hover-shine">
+                                <div class="avatar-icon rounded">
+                                    <img src="/images/avatars/3.jpg" alt="Avatar 5">
+                                </div>
+                            </div>
+                            <div>
+                                <h5 class="menu-header-title">{{ $agent_name }}</h5>
+                                <h6 class="menu-header-subtitle">Agent</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>                    
+                <ul class="list-group list-group-flush">
+                    @foreach($agent_sites as $what_site)
+                    <li class="list-group-item">
+                        <div class="widget-content p-0">
+                            <div class="widget-content-wrapper">
+                                <div class="widget-content-left mr-3">
+                                    <div class="icon-wrapper m-0">
+                                        <div class="progress-circle-wrapper">
+                                            <div class="circle-progress d-inline-block circle-progress-success-sm">
+                                                <small><span>81%<span></span></span></small>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="widget-content-left">
+                                    <div class="widget-heading"><a href="{{ route('view_assigned_site',[$what_site->sam_id]) }}">{{ $what_site->site_name }}</a></div>
+                                    <div class="widget-subheading">{{ $what_site->sam_id }}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>                        
+                    @endforeach
+                </ul>
+            </div>
     </div>
 
     <input id="timeline" type="hidden" value="{{ $timeline }}" />
