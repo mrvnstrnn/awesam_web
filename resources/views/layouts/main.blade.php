a4440222166560655ddf65d5c696ef5d8fb02ac8
Marvin Esternon <maesternon@absi.ph>
2021-06-10 08:57:08 +0800

    upload feature

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -26,6 +26,8 @@
 
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
         <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.9.2/dropzone.min.css" integrity="sha512-jU/7UFiaW5UBGODEopEqnbIAHOI8fO6T99m7Tsmqs2gkdujByJfkCbbfPSN4Wlqlb9TGnsuC0YgUgWkRBK7B9A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         
 
         <style>
