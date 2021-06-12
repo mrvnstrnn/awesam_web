08b4c5f9d6f4a41c7987d3b902f4515223609102
Aris Salvador <zfdsalvador@globe.com.ph>
2021-06-12 10:48:06 +0800

    Program Sites

---
+++ resources/views/profiles/globe/program-staff/program-sites.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('content')
+
+    <x-assigned-sites />
+
+@endsection
+
+@section('js_script')
+
+    <script type="text/javascript" src="/js/assigned-sites.js"></script>  
+
+@endsection
