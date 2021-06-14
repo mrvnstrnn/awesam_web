8cc4430347dcf4690798b25d5898ca67da41e7bf
Fernando Salvador <fernandosalvador@Fernandos-MacBook-Pro.local>
2021-06-14 16:34:11 +0800

    Program Head Site Approvals

---
+++ resources/views/profiles/globe/program-head/site-approvals.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.main')
+
+@section('content')
+
+    <x-doc-validation />
+
+@endsection
+
+
+@section('modals')
+
+<div class="modal fade" id="viewInfoModal" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true" >
+    <div class="modal-dialog modal-xl" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+
+            </div>
+            <div class="modal-body">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-link" data-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-danger btn_reject_approve" data-action="rejected">Reject</button>
+                <button type="button" class="btn btn-primary btn_reject_approve" data-action="approved">Approve</button>        
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+
+@section('js_script')
+
+    <script type="text/javascript" src="/js/getCols.js"></script>  
+    <script type="text/javascript" src="/js/doc-validations.js"></script>  
+
+@endsection
