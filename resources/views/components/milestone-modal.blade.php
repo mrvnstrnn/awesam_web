e809d81170400cb6cf2d9ed5504281af8fe39753
Fernando Salvador <fernandosalvador@Fernandos-MacBook-Pro.local>
2021-06-16 18:47:43 +0800

    Doc Validation Preview Modal

--- resources/views/components/milestone-modal.blade.php
+++ resources/views/components/milestone-modal.blade.php
@@ -10,7 +10,7 @@
             <div class="modal-body">
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                 {{-- <button type="button" class="btn btn-danger btn_reject_approve" data-action="rejected">Reject</button>
                 <button type="button" class="btn btn-primary btn_reject_approve" data-action="approved">Approve</button>         --}}
             </div>
