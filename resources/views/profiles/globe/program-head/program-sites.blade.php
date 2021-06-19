cb5312c3eb5630dc7f4e7a7214ca89799c549848
Fernando Salvador <fernandosalvador@Fernandos-MacBook-Pro.local>
2021-06-20 00:04:08 +0800

    save

--- resources/views/profiles/globe/program-head/program-sites.blade.php
+++ resources/views/profiles/globe/program-head/program-sites.blade.php
@@ -2,9 +2,11 @@
 
 @section('content')
 
-    <x-assigned-sites mode="globe"/>
+    {{-- <x-assigned-sites mode="vendor"/> --}}
+    <x-milestone-datatable ajaxdatatablesource="site-milestones" tableheader="Program Sites" activitytype="all"/>
 
 @endsection
+
 
 @section('modals')
 
@@ -17,15 +19,14 @@
 
 <script>
     //////////////////////////////////////
-    var profile_id = 10;
-    var table_to_load = 'assigned_sites';
+    var profile_id = 7;
+    var table_to_load = 'program_sites';
     //////////////////////////////////////
 </script>
 
 <script type="text/javascript" src="/js/getCols.js"></script>  
 <script type="text/javascript" src="/js/DTmaker.js"></script>  
 <script type="text/javascript" src="/js/modal-loader.js"></script>  
-
 
 
 @endsection
