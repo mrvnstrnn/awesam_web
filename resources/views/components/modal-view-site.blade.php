4051c8f868ee55a3eba4e05a8506602afdb0fbc9
Fernando Salvador <fernandosalvador@Fernandos-MacBook-Pro.local>
2021-06-24 17:59:13 +0800

    ajax

--- resources/views/components/modal-view-site.blade.php
+++ resources/views/components/modal-view-site.blade.php
@@ -233,7 +233,26 @@
                         </div> 
                     </div>
                 
-                    <div class="col-lg-4 col-md-12 col-sm-12" id="modal_site_right">
+                    <div class="col-lg-4 col-md-12 col-sm-12">
+
+                        <div class="mb-3 profile-responsive card">
+                            <div class="dropdown-menu-header">
+                                <div class="dropdown-menu-header-inner bg-dark">
+                                    <div class="menu-header-image opacity-2" style="background-image: url('/images/dropdown-header/abstract2.jpg'); background-size: cover;"></div>
+                                    <div class="menu-header-content btn-pane-right">
+                                        <div>
+                                            <h5 class="menu-header-title">Site Status</h5>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="p-0 card-body align-center text-center py-5" style="position: relative;">                        
+                                <div id="modal_site_right">
+                                </div>
+                            </div>
+                        </div> 
+
+                        
                         {{-- <x-site-status :completed="35" :samid="$site[0]->sam_id"/> --}}
                     </div>                
                 </div>
--- resources/views/components/site-status.blade.php
+++ resources/views/components/site-status.blade.php
@@ -9,24 +9,10 @@
 @endphp
 
 
-<div class="mb-3 profile-responsive card">
-    <div class="dropdown-menu-header">
-        <div class="dropdown-menu-header-inner bg-dark">
-            <div class="menu-header-image opacity-2" style="background-image: url('/images/dropdown-header/abstract2.jpg'); background-size: cover;"></div>
-            <div class="menu-header-content btn-pane-right">
-                <div>
-                    <h5 class="menu-header-title">Site Status</h5>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="p-0 card-body align-center text-center py-5" style="position: relative;">
 
         <div class="circle-progress circle-progress-modal d-inline-block">
             <small><span class="site_progress_modal">{{ $site_status[0]->progress }}</span></small>
         </div>
-    </div>
-</div> 
 
 {{-- <div class="card-shadow-primary profile-responsive card-border mb-3 card">
     <div class="dropdown-menu-header">
