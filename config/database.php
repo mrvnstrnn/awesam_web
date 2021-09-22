16e430004dc7f44e667dbf9de7616189ca0e29bc
Aris Salvador <82378038+zfdsalvador@users.noreply.github.com>
2021-09-22 16:34:05 +0800

    DB

--- config/database.php
+++ config/database.php
@@ -49,13 +49,8 @@ return [
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'samtool'),
-<<<<<<< HEAD
             'username' => env('DB_USERNAME', 'liveserver'),
             'password' => env('DB_PASSWORD', 'P_U9*F)@f9&K5(W:'),
-=======
-            'username' => env('DB_USERNAME', 'marvin'),
-            'password' => env('DB_PASSWORD', 'X9Ed&Zchmksts39!'),
->>>>>>> a9baf103e5416ef6ecc509d5ca6a66428712904e
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
