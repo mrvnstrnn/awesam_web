8674cfff5dafd11923feb781c956866a6ae2bf7f
Marvin Esternon <maesternon@absi.ph>
2021-09-23 15:47:31 +0800

    update

--- resources/views/profiles/vendor/vendor-admin/unassigned-sites.blade.php
+++ resources/views/profiles/vendor/vendor-admin/unassigned-sites.blade.php
@@ -10,26 +10,39 @@
 
 @section('modals')
 
-<div class="modal fade" id="modal-assign-sites" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true" data-backdrop="static" data-keyboard="false">
-    <div class="modal-dialog modal-dialog-scrollable" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Assign Sites</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <form id="agent_form">
-                <div class="modal-body" style="overflow-y: auto !important; max-height: calc(100vh - 210px);">
-                    <div class="form-row">
-                    <input type="hidden" id="sam_id" name="sam_id">
-                        <select name="agent_id" id="agent_id" class="form-control"></select>
+<div class="modal fade" id="modal-assign-sites" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true"  data-keyboard="false">
+    <div class="modal-dialog modal-md" role="document">
+        <div class="modal-content" style="background-color: transparent; border: 0">
+            <div class="row justify-content-center">
+                <div class="col-lg-12 col-md-12 col-sm-12">
+                    <div class="main-card mb-3 card ">
+
+                        <div class="dropdown-menu-header">
+                            <div class="dropdown-menu-header-inner bg-dark">
+                                <div class="menu-header-image opacity-2" style="background-image: url('/images/dropdown-header/abstract2.jpg');"></div>
+                                <div class="menu-header-content btn-pane-right">
+                                    <h5 class="menu-header-title">
+                                        Assign Sites
+                                    </h5>
+                                </div>
+                            </div>
+                        </div> 
+
+                        <form id="agent_form">
+                            <div class="modal-body" style="overflow-y: auto !important; max-height: calc(100vh - 210px);">
+                                <div class="form-row">
+                                <input type="hidden" id="sam_id" name="sam_id">
+                                    <select name="agent_id" id="agent_id" class="form-control"></select>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-primary" id="btn-assign-sites" data-href="{{ route('assign.agent') }}" data-activity_name="site_assign">Assign</button>
+                            </div>
+                        </form>
+
                     </div>
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-primary" id="btn-assign-sites" data-href="{{ route('assign.agent') }}" data-activity_name="site_assign">Assign</button>
-                </div>
-            </form>
+            </div>
         </div>
     </div>
 </div>
