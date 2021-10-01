a0d93052065228c10a76df8404b52b2060eee065
Aris Salvador <82378038+zfdsalvador@users.noreply.github.com>
2021-10-01 14:24:49 +0800

    supervisor

--- resources/views/profiles/vendor/supervisor/index.blade.php
+++ resources/views/profiles/vendor/supervisor/index.blade.php
@@ -1,56 +1,65 @@
 @extends('layouts.home')
 
+@section('style')
+    <style>
+        .offline {
+            -webkit-filter: grayscale(100%);
+            filter: grayscale(100%);        
+        }
+    </style>
+@endsection
+
 @section('content')
 
-    <H1>Good Afternoon <strong>{{ ucwords(\Auth::user()->firstname) }}</strong> </H1>
-    <hr>
-    <h3>Sites</h3>
-    <div class="row">
-        <div class="col-md-6 col-lg-3">
-            <div class="mb-3 card">
-                <div class="widget-chart widget-chart2 text-left p-0">
-                    <div class="widget-chat-wrapper-outer">
-                        <div class="widget-chart-content widget-chart-content-lg milestone-submilestones">
-                            <div class="milestone-bg" style="position: absolute; left: 0px; top:0px; opacity: 0.30; height: 100%; width:100%; background-image: url('/images/milestone-gray.jpeg');   background-repeat: no-repeat; background-size: 100%;"></div>
+<H1>Good Afternoon <strong>{{ ucfirst(\Auth::user()->firstname) }}</strong> </H1>
+<hr>
+<h3>Sites</h3>
+<div class="row">
+    <div class="col-md-6 col-lg-3">
+        <div class="mb-3 card">
+            <div class="widget-chart widget-chart2 text-left p-0">
+                <div class="widget-chat-wrapper-outer">
+                    <div class="widget-chart-content widget-chart-content-lg milestone-submilestones">
+                        <div class="milestone-bg" style="position: absolute; left: 0px; top:0px; opacity: 0.30; height: 100%; width:100%; background-image: url('/images/milestone-gray.jpeg');   background-repeat: no-repeat; background-size: 100%;"></div>
 
-                            <div class="widget-chart-flex">
-                                <div class="widget-title  text-muted text-uppercase">
-                                    Assigned Sites
-                                </div>
+                        <div class="widget-chart-flex">
+                            <div class="widget-title  text-muted text-uppercase">
+                                Assigned Sites
                             </div>
-                            <div class="widget-numbers">
-                                <span class="opacity-10 text-secondary pr-2">
-                                    <i class="fa fa-upload"></i>
-                                </span>
-                                <span>1</span>
-                            </div>
+                        </div>
+                        <div class="widget-numbers">
+                            <span class="opacity-10 text-secondary pr-2">
+                                <i class="fa fa-upload"></i>
+                            </span>
+                            <span>1</span>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="col-md-6 col-lg-3">
-            <div class="mb-3 card">
-                <div class="widget-chart widget-chart2 text-left p-0">
-                    <div class="widget-chat-wrapper-outer">
-                        <div class="widget-chart-content widget-chart-content-lg milestone-submilestones" data-modal_title="Pre-BRGY Distribution" data-modal_ul="Pre_BRGY_Distribution">
-                            <div class="milestone-bg" style="position: absolute; left: 0px; top:0px; opacity: 0.30; height: 100%; width:100%; background-image: url('/images/milestone-primary.jpeg');   background-repeat: no-repeat; background-size: 100%;"></div>
+    </div>
+    <div class="col-md-6 col-lg-3">
+        <div class="mb-3 card">
+            <div class="widget-chart widget-chart2 text-left p-0">
+                <div class="widget-chat-wrapper-outer">
+                    <div class="widget-chart-content widget-chart-content-lg milestone-submilestones" data-modal_title="Pre-BRGY Distribution" data-modal_ul="Pre_BRGY_Distribution">
+                        <div class="milestone-bg" style="position: absolute; left: 0px; top:0px; opacity: 0.30; height: 100%; width:100%; background-image: url('/images/milestone-primary.jpeg');   background-repeat: no-repeat; background-size: 100%;"></div>
 
-                            <div class="widget-chart-flex">
-                                <div class="widget-title opacity-5 text-muted text-uppercase">
-                                    New Endorsements
-                                </div>
+                        <div class="widget-chart-flex">
+                            <div class="widget-title opacity-5 text-muted text-uppercase">
+                                New Endorsements
                             </div>
-                            <div class="widget-numbers">
-                                <span class="opacity-10 text-info">
-                                    <i class="fa fa-list-ol"></i>
-                                </span>
-                                <span>0</span>
-                            </div>
+                        </div>
+                        <div class="widget-numbers">
+                            <span class="opacity-10 text-info">
+                                <i class="fa fa-list-ol"></i>
+                            </span>
+                            <span>0</span>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
     </div>
     <div class="col-md-6 col-lg-3">
         <div class="mb-3 card">
@@ -107,8 +116,59 @@
             </div>
         </div>
     </div>
-
 </div>
+<h3>My Team</h3>
+<div class="row" style="margin-top: 30px;">
+    <div class="col-md-6 col-lg-2">
+        <div class="widget-content-left mr-3" style="text-align: center;">
+            <img class="rounded-circle" src="images/avatars/1.jpg" alt="" width="70">
+        </div>
+        <div style="text-align: center;">
+            <small>Test</small>
+        </div>
+    </div>
+    <div class="col-md-6 col-lg-2">
+        <div class="widget-content-left mr-3" style="text-align: center;">
+            <img class="offline rounded-circle" src="images/avatars/2.jpg" alt="" width="70">
+        </div>
+        <div style="text-align: center;">
+            <small>Test</small>
+        </div>
+    </div>
+    <div class="col-md-6 col-lg-2">
+        <div class="widget-content-left mr-3" style="text-align: center;">
+            <img class="rounded-circle offline" src="images/avatars/3.jpg" alt="" width="70">
+        </div>
+        <div style="text-align: center;">
+            <small>Test</small>
+        </div>
+    </div>
+    <div class="col-md-6 col-lg-2">
+        <div class="widget-content-left mr-3" style="text-align: center;">
+            <img class="rounded-circle" src="images/avatars/4.jpg" alt="" width="70">
+        </div>
+        <div style="text-align: center;">
+            <small>Test</small>
+        </div>
+    </div>
+    <div class="col-md-6 col-lg-2">
+        <div class="widget-content-left mr-3" style="text-align: center;">
+            <img class="rounded-circle" src="images/avatars/5.jpg" alt="" width="70">
+        </div>
+        <div style="text-align: center;">
+            <small>Test</small>
+        </div>
+    </div>
+    <div class="col-md-6 col-lg-2">
+        <div class="widget-content-left mr-3" style="text-align: center;">
+            <img class="rounded-circle offline" src="images/avatars/6.jpg" alt="" width="70">
+        </div>
+        <div style="text-align: center;">
+            <small>Test</small>
+        </div>
+    </div>
+</div>
+
 @endsection
 
 @section("js_script")
