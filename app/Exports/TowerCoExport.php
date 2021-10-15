090c9dd3e53cde2e9a0d37e61eb6d8c88b98f60f
Marvin Esternon <maesternon@absi.ph>
2021-10-15 14:42:36 +0800

    update

--- app/Exports/TowerCoExport.php
+++ app/Exports/TowerCoExport.php
@@ -32,109 +32,119 @@ class TowerCoExport implements FromCollection, WithHeadings
         //                 $select_collect->all()
         //             );
 
-        $towerco = TowerCo::select(
-            "Serial Number", 
-            "Search Ring",
-            "REGION",
-            "TOWERCO",
-            // "DATE ENDORSED BY RAM",
-            "MLA COMPLETION DATE",
-            "DATE ACCEPTED BY TOWERCO",
-            // "SITE ACQUIRED FORECAST",
-            // "SITE DATE ACQUIRED",
-            // "ESTIMATED RFI DATE",
-            "PROJECT TAG",
-            "MILESTONE STATUS",
-            "PROVINCE",
-            "TOWN",
-            "[NP]Latitude",
-            "[NP]Longitude",
-            "SITE TYPE",
-            "Tower Height",
-            "FOC/ MW TAGGING",
-            "Wind Speed",
-            "OFF-GRID/GOOD GRID",
-            // "BATCH",
-            "Tower Co TSSR Submission Date to GT",
-            "TSSR STATUS",
-            "TSSR APPROVED DATE",
-            "TSSR SUBMIT DATE",
-            "CW START DATE",
-            "CW COMPLETED DATE",
-            "RFI DATE SUBMITTED",
-            // "RFI DATE APPROVED (TEMPO POWER)",
-            "RFI DATE APPROVED (PERMANENT POWER)",
-            "PRIO",
-            "LOT SIZE (sq-m)",
-            "ACCESS",
-            "LINK TO DOCS FOR GLOBE-ACQUIRED SITES",
-            "LANDLORD INFO",
-            "LEASE AMOUNT",
-            "LEASE ESCALATION",
-            "LEASE TERM",
-            // "BASE LEASE FEE",
-            // "ESCALATION",
-            // "COMMENCEMENT",
-            "REMARKS"
-        );
+        // $towerco = TowerCo::select(
+        //     "Serial Number", 
+        //     "Search Ring",
+        //     "REGION",
+        //     "TOWERCO",
+        //     // "DATE ENDORSED BY RAM",
+        //     "MLA COMPLETION DATE",
+        //     "DATE ACCEPTED BY TOWERCO",
+        //     // "SITE ACQUIRED FORECAST",
+        //     // "SITE DATE ACQUIRED",
+        //     // "ESTIMATED RFI DATE",
+        //     "PROJECT TAG",
+        //     "MILESTONE STATUS",
+        //     "PROVINCE",
+        //     "TOWN",
+        //     "[NP]Latitude",
+        //     "[NP]Longitude",
+        //     "SITE TYPE",
+        //     "Tower Height",
+        //     "FOC/ MW TAGGING",
+        //     "Wind Speed",
+        //     "OFF-GRID/GOOD GRID",
+        //     // "BATCH",
+        //     "Tower Co TSSR Submission Date to GT",
+        //     "TSSR STATUS",
+        //     "TSSR APPROVED DATE",
+        //     "TSSR SUBMIT DATE",
+        //     "CW START DATE",
+        //     "CW COMPLETED DATE",
+        //     "RFI DATE SUBMITTED",
+        //     // "RFI DATE APPROVED (TEMPO POWER)",
+        //     "RFI DATE APPROVED (PERMANENT POWER)",
+        //     "PRIO",
+        //     "LOT SIZE (sq-m)",
+        //     "ACCESS",
+        //     "LINK TO DOCS FOR GLOBE-ACQUIRED SITES",
+        //     "LANDLORD INFO",
+        //     "LEASE AMOUNT",
+        //     "LEASE ESCALATION",
+        //     "LEASE TERM",
+        //     // "BASE LEASE FEE",
+        //     // "ESCALATION",
+        //     // "COMMENCEMENT",
+        //     "REMARKS"
+        // );
 
-        if (\Auth::user()->profile_id == 21) {
-            $user_detail = \Auth::user()->getUserDetail()->first();
-            $vendor = Vendor::where('vendor_id', $user_detail->vendor_id)->first();
+        // if (\Auth::user()->profile_id == 21) {
+        //     $user_detail = \Auth::user()->getUserDetail()->first();
+        //     $vendor = Vendor::where('vendor_id', $user_detail->vendor_id)->first();
 
-            $towerco->where('TOWERCO', $vendor->vendor_acronym);
-        }
+        //     $towerco->where('TOWERCO', $vendor->vendor_acronym);
+        // }
 
-        return $towerco->get();
+        return $towerco = TowerCo::get();
     }
+
+    // public function headings() :array
+    // {
+    //     return [
+    //         "Serial Number", 
+    //         "Search Ring",
+    //         "REGION",
+    //         "TOWERCO",
+    //         // "DATE ENDORSED BY RAM",
+    //         "MLA COMPLETION DATE",
+    //         "DATE ACCEPTED BY TOWERCO",
+    //         // "SITE ACQUIRED FORECAST",
+    //         // "SITE DATE ACQUIRED",
+    //         // "ESTIMATED RFI DATE",
+    //         "PROJECT TAG",
+    //         "MILESTONE STATUS",
+    //         "PROVINCE",
+    //         "TOWN",
+    //         "[NP]Latitude",
+    //         "[NP]Longitude",
+    //         "SITE TYPE",
+    //         "Tower Height",
+    //         "FOC/ MW TAGGING",
+    //         "Wind Speed",
+    //         "OFF-GRID/GOOD GRID",
+    //         // "BATCH",
+    //         "Tower Co TSSR Submission Date to GT",
+    //         "TSSR STATUS",
+    //         "TSSR APPROVED DATE",
+    //         "TSSR SUBMIT DATE",
+    //         "CW START DATE",
+    //         "CW COMPLETED DATE",
+    //         "RFI DATE SUBMITTED",
+    //         // "RFI DATE APPROVED (TEMPO POWER)",
+    //         "RFI DATE APPROVED (PERMANENT POWER)",
+    //         "PRIO",
+    //         "LOT SIZE (sq-m)",
+    //         "ACCESS",
+    //         "LINK TO DOCS FOR GLOBE-ACQUIRED SITES",
+    //         "LANDLORD INFO",
+    //         "LEASE AMOUNT",
+    //         "LEASE ESCALATION",
+    //         "LEASE TERM",
+    //         // "BASE LEASE FEE",
+    //         // "ESCALATION",
+    //         // "COMMENCEMENT",
+    //         "REMARKS"
+    //     ];
+    // }
 
     public function headings() :array
     {
-        return [
-            "Serial Number", 
-            "Search Ring",
-            "REGION",
-            "TOWERCO",
-            // "DATE ENDORSED BY RAM",
-            "MLA COMPLETION DATE",
-            "DATE ACCEPTED BY TOWERCO",
-            // "SITE ACQUIRED FORECAST",
-            // "SITE DATE ACQUIRED",
-            // "ESTIMATED RFI DATE",
-            "PROJECT TAG",
-            "MILESTONE STATUS",
-            "PROVINCE",
-            "TOWN",
-            "[NP]Latitude",
-            "[NP]Longitude",
-            "SITE TYPE",
-            "Tower Height",
-            "FOC/ MW TAGGING",
-            "Wind Speed",
-            "OFF-GRID/GOOD GRID",
-            // "BATCH",
-            "Tower Co TSSR Submission Date to GT",
-            "TSSR STATUS",
-            "TSSR APPROVED DATE",
-            "TSSR SUBMIT DATE",
-            "CW START DATE",
-            "CW COMPLETED DATE",
-            "RFI DATE SUBMITTED",
-            // "RFI DATE APPROVED (TEMPO POWER)",
-            "RFI DATE APPROVED (PERMANENT POWER)",
-            "PRIO",
-            "LOT SIZE (sq-m)",
-            "ACCESS",
-            "LINK TO DOCS FOR GLOBE-ACQUIRED SITES",
-            "LANDLORD INFO",
-            "LEASE AMOUNT",
-            "LEASE ESCALATION",
-            "LEASE TERM",
-            // "BASE LEASE FEE",
-            // "ESCALATION",
-            // "COMMENCEMENT",
-            "REMARKS"
-        ];
+
+        $x = \DB::getSchemaBuilder()->getColumnListing('towerco');
+
+        return $x;
+
+
     }
  
 }
