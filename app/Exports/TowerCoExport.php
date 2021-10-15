d9eab20eda9314500f787664ee12fc7d355c2c0b
Aris Salvador <zfdsalvador@globe.com.ph>
2021-10-15 14:45:03 +0800

    tower

--- app/Exports/TowerCoExport.php
+++ app/Exports/TowerCoExport.php
@@ -16,51 +16,7 @@ class TowerCoExport implements FromCollection, WithHeadings
     public function collection()
     {
 
-        $towerco = TowerCo::select(
-            "Serial Number", 
-            "Search Ring",
-            "REGION",
-            "TOWERCO",
-            "DATE ENDORSED BY RAM",
-            "MLA COMPLETION DATE",
-            "DATE ACCEPTED BY TOWERCO",
-            "SITE ACQUIRED FORECAST",
-            "SITE DATE ACQUIRED",
-            "ESTIMATED RFI DATE",
-            "PROJECT TAG",
-            "MILESTONE STATUS",
-            "PROVINCE",
-            "TOWN",
-            "[NP]Latitude",
-            "[NP]Longitude",
-            "SITE TYPE",
-            "Tower Height",
-            "FOC/ MW TAGGING",
-            "Wind Speed",
-            "OFF-GRID/GOOD GRID",
-            "BATCH",
-            "Tower Co TSSR Submission Date to GT",
-            "TSSR STATUS",
-            "TSSR APPROVED DATE",
-            "TSSR SUBMIT DATE",
-            "CW START DATE",
-            "CW COMPLETED DATE",
-            "RFI DATE SUBMITTED",
-            "RFI DATE APPROVED (TEMPO POWER)",
-            "RFI DATE APPROVED (PERMANENT POWER)",
-            "PRIO",
-            "LOT SIZE (sq-m)",
-            "ACCESS",
-            "LINK TO DOCS FOR GLOBE-ACQUIRED SITES",
-            "LANDLORD INFO",
-            "LEASE AMOUNT",
-            "LEASE ESCALATION",
-            "LEASE TERM",
-            "BASE LEASE FEE",
-            "ESCALATION",
-            "COMMENCEMENT",
-            "REMARKS"
-        );
+        $towerco = \DB::table('towerco');
 
         
         if (\Auth::user()->profile_id == 21) {
@@ -75,51 +31,12 @@ class TowerCoExport implements FromCollection, WithHeadings
 
     public function headings() :array
     {
-        return [
-            "Serial Number", 
-            "Search Ring",
-            "REGION",
-            "TOWERCO",
-            "DATE ENDORSED BY RAM",
-            "MLA COMPLETION DATE",
-            "DATE ACCEPTED BY TOWERCO",
-            "SITE ACQUIRED FORECAST",
-            "SITE DATE ACQUIRED",
-            "ESTIMATED RFI DATE",
-            "PROJECT TAG",
-            "MILESTONE STATUS",
-            "PROVINCE",
-            "TOWN",
-            "[NP]Latitude",
-            "[NP]Longitude",
-            "SITE TYPE",
-            "Tower Height",
-            "FOC/ MW TAGGING",
-            "Wind Speed",
-            "OFF-GRID/GOOD GRID",
-            "BATCH",
-            "Tower Co TSSR Submission Date to GT",
-            "TSSR STATUS",
-            "TSSR APPROVED DATE",
-            "TSSR SUBMIT DATE",
-            "CW START DATE",
-            "CW COMPLETED DATE",
-            "RFI DATE SUBMITTED",
-            "RFI DATE APPROVED (TEMPO POWER)",
-            "RFI DATE APPROVED (PERMANENT POWER)",
-            "PRIO",
-            "LOT SIZE (sq-m)",
-            "ACCESS",
-            "LINK TO DOCS FOR GLOBE-ACQUIRED SITES",
-            "LANDLORD INFO",
-            "LEASE AMOUNT",
-            "LEASE ESCALATION",
-            "LEASE TERM",
-            "BASE LEASE FEE",
-            "ESCALATION",
-            "COMMENCEMENT",
-            "REMARKS"
-        ];
+
+        $x = \DB::getSchemaBuilder()->getColumnListing('towerco');
+
+        return $x;
+
+
     }
  
 }
