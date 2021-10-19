a3905f876e45bd11019a8af8a26bac97f3f3fbe7
Aris Salvador <zfdsalvador@globe.com.ph>
2021-10-19 12:07:14 +0800

    Youtube

--- resources/views/training.blade.php
+++ resources/views/training.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.training')
 
 @section('content')
+<div class="row">
+    </div class="col">
+        <H5>Vendor Admin Training</H5>
+        <iframe width="100%" height="600" src="https://www.youtube.com/embed/xM1suC_3z28" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    </div>
+</div>
 
 @endsection
