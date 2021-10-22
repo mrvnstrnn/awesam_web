8a449f406252f18947bb8b04f4029e758fda2958
Marvin Esternon <maesternon@absi.ph>
2021-10-22 15:39:32 +0800

    update

--- app/Http/Controllers/GlobeController.php
+++ app/Http/Controllers/GlobeController.php
@@ -327,7 +327,6 @@ class GlobeController extends Controller
 
             } else if ($request->input('activity_name') == "mark_as_complete") {
 
-                return response()->json(['error' => true, 'message' => $request->all() ]);
                 $notification = "Successfully mark this site as completed.";
                 $action = "true";
                 $activity_id = $request->input('activity_id');
