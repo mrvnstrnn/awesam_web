d069de31bc15a1af7a8c33c1d4982502b5759f1d
Aris Salvador <zfdsalvador@globe.com.ph>
2021-10-27 08:38:36 +0800

    s

--- resources/views/components/btn-show-site-details.blade.php
+++ resources/views/components/btn-show-site-details.blade.php
@@ -1,5 +1,5 @@
 <div class="row pt-3 border-top"> 
-    <div class="col-6 text-left">
+    <div class="col-12 text-center">
         <button class="btn-icon btn-pill btn btn-med btn-focus" id="site_action_view_switch">
             <i class="pe-7s-angle-down-circle pe-3x pe-va btn-icon-wrapper"></i>Show Site Details
         </button>
--- resources/views/components/site-modal-action-view.blade.php
+++ resources/views/components/site-modal-action-view.blade.php
@@ -122,13 +122,9 @@
 
     </div>
     <x-btn-show-site-details />
-    {{-- <div class="row pt-3 border-top"> 
-        <div class="col-12 text-center">
-            <button class="btn-icon btn-pill btn btn-lg btn-focus" id="site_action_view_switch">
-                <i class="pe-7s-angle-down-circle pe-3x pe-va btn-icon-wrapper"></i>Show Site Details
-            </button>
-        </div>
-    </div> --}}
+</div>
+<div id="site_details_view" class="d-none">
+    <x-site-modal-details-view :site="$site" :mainactivity="$mainactivity"/>
 </div>
 
 <script>
