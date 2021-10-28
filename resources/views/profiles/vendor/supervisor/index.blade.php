9c0b9b62433568bb0c23558da5297802790d1be7
Aris Salvador <zfdsalvador@globe.com.ph>
2021-10-28 13:00:49 +0800

    s

--- resources/views/profiles/vendor/supervisor/index.blade.php
+++ resources/views/profiles/vendor/supervisor/index.blade.php
@@ -117,7 +117,7 @@
 <div class="divider"></div>
 <div class="row">
     <div class="col-12">
-        <h3>Agent Dashboard</h3>
+        <h3>Team Dashboard</h3>
     </div>
 </div>
 <div class="mb-3 card">
