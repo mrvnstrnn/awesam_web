c50bcccd104b2eb406c855a27104caf17edf14ca
Aris Salvador <zfdsalvador@globe.com.ph>
2021-11-04 08:16:53 +0800

    dd

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -40,18 +40,10 @@ class ActivityController extends Controller
                         
                 if ( is_null($get_component) ) {
 
-                    // return \View::make('components.activity-error')
-                    //         ->with([
-                    //             'site' => $site,
-                    //             'activity_source' => $request->get('activity_source'),  
-                    //             'main_activity' => '',
-                    //         ])
-                    //         ->render();
-
-                    return \View::make('components.modal-info-site')
+                    return \View::make('components.activity-error')
                             ->with([
                                 'site' => $site,
-                                'activity_source' => $request->get('activity_source'),
+                                'activity_source' => $request->get('activity_source'),  
                                 'main_activity' => '',
                             ])
                             ->render();
@@ -115,7 +107,7 @@ class ActivityController extends Controller
                         break;
 
                     default: 
-                    
+
                         $component_to_load = "activity-error";
 
                 }
