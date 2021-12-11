21195997df7512af6361f86e1cbe2b7e073e513e
zx016536 <zx016536@globetel.com>
2021-12-11 19:51:58 +0800

    update

--- public/js/supervisor.js
+++ public/js/supervisor.js
@@ -58,6 +58,39 @@ $(document).ready(() => {
             ],
         });
 
+        $('#vendor-admin-'+program_lists[0]+'-table').DataTable({
+            processing: true,
+            serverSide: true,
+            // pageLength: 3,
+            ajax: {
+                url: $('#vendor-admin-'+program_lists[0]+'-table').attr('data-href'),
+                type: 'POST',
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+                data: {
+                    program_id : program_id
+                }
+            },
+            dataSrc: function(json){
+                return json.data;
+            },
+            'createdRow': function( row, data, dataIndex ) {
+                $(row).attr('data-id', data.id);
+            },
+            columnDefs: [{
+                "targets": 0,
+                "orderable": false
+            }],
+            columns: [
+                { data: "photo" },
+                { data: "firstname" },
+                { data: "lastname" },
+                { data: "email" },
+                { data: "areas" },
+            ],
+        });
+
         $('#newagent-'+program_lists[0]+'-table').DataTable({
             processing: true,
             serverSide: true,
@@ -117,6 +150,40 @@ $(document).ready(() => {
                         $(row).addClass('modalAssigAgentnSite');
                         $(row).attr('data-program', data_program);
                     }
+                    $(row).attr('style', "cursor: pointer");
+                },
+                columnDefs: [{
+                    "targets": 0,
+                    "orderable": false
+                }],
+                columns: [
+                    { data: "photo" },
+                    { data: "firstname" },
+                    { data: "lastname" },
+                    { data: "email" },
+                    { data: "areas" },
+                ],
+            });
+        }
+
+        if ( ! $.fn.DataTable.isDataTable('#vendor-admin-'+$(this).attr("data-program")+'-table') ) {
+            var data_program = $(this).attr("data-program");
+            $('#vendor-admin-'+data_program+'-table').DataTable({
+                processing: true,
+                serverSide: true,
+                // pageLength: 3,
+                ajax: {
+                    url: $('#vendor-admin-'+data_program+'-table').attr('data-href'),
+                    type: 'GET',
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
+                },
+                dataSrc: function(json){
+                    return json.data;
+                },
+                'createdRow': function( row, data, dataIndex ) {
+                    $(row).attr('data-id', data.id);
                     $(row).attr('style', "cursor: pointer");
                 },
                 columnDefs: [{
---
+++ resources/views/profiles/vendor/vendor-admin/vendor-admin.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.main')
+
+@section('content')
+    <style>
+        .modalDataEndorsement {
+            cursor: pointer;
+        }
+
+        table {
+            width: 100% !important;
+        }
+    </style> 
+
+    <ul class="tabs-animated body-tabs-animated nav">
+
+        @php
+            // $programs = App\Models\VendorProgram::orderBy('vendor_program')->get();
+            $programs = \Auth::user()->getUserProgram();
+        @endphp
+
+<input type="hidden" name="program_lists" id="program_lists" value="{{ json_encode($programs) }}">
+
+        @foreach ($programs as $program)
+            <li class="nav-item">
+                <a role="tab" class="nav-link agent {{ $loop->first ? 'active' : '' }}" id="tab-{{ $program->program_id  }}" data-toggle="tab" href="#tab-content-{{ $program->program_id  }}" data-program="{{ strtolower(str_replace(" ", "-", $program->program))  }}">
+                    <span>{{ $program->program }}</span>
+                </a>
+            </li>
+        @endforeach
+    </ul>
+    <div class="tab-content">
+        @foreach ($programs as $program)
+            <div class="tab-pane tabs-animation fade {{ $loop->first ? 'active show' : '' }}" id="tab-content-{{ $program->program_id  }}" role="tabpanel">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="main-card mb-3 card">
+                            <div class="card-header-tab card-header">
+                                <div class="card-header-title font-size-lg text-capitalize font-weight-normal">
+                                <i class="header-icon lnr-layers icon-gradient bg-ripe-malin"></i>
+                                {{ strtoupper($program->program)  }} Agents
+                                </div>      
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table id="vendor-admin-{{ strtolower(str_replace(" ", "-", $program->program))  }}-table" class="align-middle mb-0 table table-borderless table-striped table-hover unasigned-table new-endorsement-table" data-href="{{ route('vendor_admin', $program->program_id) }}" data-program_id="{{ $program->program_id }}">
+                                        <thead>
+                                            <tr>
+                                                <th style="width: 10%;">Photo</th>
+                                                <th>Firstname</th>
+                                                <th>Lastname</th>
+                                                <th>Email</th>
+                                                <th>Areas</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+@endsection
+
+@section('js_script')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/js/jquery.dataTables.min.js" integrity="sha512-BkpSL20WETFylMrcirBahHfSnY++H2O1W+UnEEO4yNIl+jI2+zowyoGJpbtk6bx97fBXf++WJHSSK2MV4ghPcg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables.net-bs4/1.10.24/dataTables.bootstrap4.min.js" integrity="sha512-NQ2u+QUFbhI3KWtE0O4rk855o+vgPo58C8vvzxdHXJZu6gLu2aLCCBMdudH9580OmLisCC1lJg2zgjcJbnBMOQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="{{ asset('js/supervisor.js') }}"></script>
+@endsection
+
+@section('modals')
+
+@endsection
