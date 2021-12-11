5da7d957ca5c66e5556b3786b95d52ec4c3298ad
zx016536 <zx016536@globetel.com>
2021-12-11 09:46:44 +0800

    update

--- config/mail.php
+++ config/mail.php
@@ -84,7 +84,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'awesamtool@globe.com.ph'),
         'name' => env('MAIL_FROM_NAME', 'aweSAM Tool'),
     ],
 
