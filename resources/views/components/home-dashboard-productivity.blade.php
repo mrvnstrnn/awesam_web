aaf577264add3e5c2ae74274a7e607edc99bd9c0
zx016536 <zx016536@globetel.com>
2021-12-15 11:34:46 +0800

    update

--- resources/views/components/home-dashboard-productivity.blade.php
+++ resources/views/components/home-dashboard-productivity.blade.php
@@ -29,42 +29,42 @@
                         <div class="milestone-bg" style="position: absolute; left: 0px; top:0px; opacity: 0.30; height: 100%; width:100%; background-image: url('/images/milestone-green-2.jpeg');   background-repeat: no-repeat; background-size: 200%;"></div>
 
                         <div class="widget-chart widget-chart-hover">
-                            <div class="widget-numbers">10</div>
+                            <div class="widget-numbers">-</div>
                             <div class="widget-subheading">Planned Activities</div>
                         </div>
                     </div>
                     <div class="col-6 col-sm-6 col-md-4 col-lg-4">
                         <div class="milestone-bg" style="position: absolute; left: 0px; top:0px; opacity: 0.30; height: 100%; width:100%; background-image: url('/images/milestone-primary.jpeg');   background-repeat: no-repeat; background-size: 200%;"></div>
                         <div class="widget-chart widget-chart-hover">
-                            <div class="widget-numbers">1</div>
+                            <div class="widget-numbers">-</div>
                             <div class="widget-subheading">Unplanned Activities</div>
                         </div>
                     </div>
                     <div class="col-6 col-sm-6 col-md-4 col-lg-4">
                         <div class="milestone-bg" style="position: absolute; left: 0px; top:0px; opacity: 0.30; height: 100%; width:100%; background-image: url('/images/milestone-orange.jpeg');   background-repeat: no-repeat; background-size: 200%;"></div>
                         <div class="widget-chart widget-chart-hover">
-                            <div class="widget-numbers">1</div>
+                            <div class="widget-numbers">-</div>
                             <div class="widget-subheading">Completed Activities</div>
                         </div>
                     </div>
                     <div class="col-6 col-sm-6 col-md-4 col-lg-4">
                         <div class="milestone-bg" style="position: absolute; left: 0px; top:0px; opacity: 0.30; height: 100%; width:100%; background-image: url('/images/milestone-orange-2.jpeg');   background-repeat: no-repeat; background-size: 200%;"></div>
                         <div class="widget-chart widget-chart-hover">
-                            <div class="widget-numbers">10%</div>
+                            <div class="widget-numbers">-</div>
                             <div class="widget-subheading">Schedule Adherence</div>
                         </div>
                     </div>
                     <div class="col-6 col-sm-6 col-md-4 col-lg-4">
                         <div class="milestone-bg" style="position: absolute; left: 0px; top:0px; opacity: 0.30; height: 100%; width:100%; background-image: url('/images/milestone-red.jpeg');   background-repeat: no-repeat; background-size: 200%;"></div>
                         <div class="widget-chart widget-chart-hover">
-                            <div class="widget-numbers">10%</div>
+                            <div class="widget-numbers">-</div>
                             <div class="widget-subheading">Planning Accuracy</div>
                         </div>
                     </div>
                     <div class="col-6 col-sm-6 col-md-4 col-lg-4">
                         <div class="milestone-bg" style="position: absolute; left: 0px; top:0px; opacity: 0.30; height: 100%; width:100%; background-image: url('/images/milestone-green.jpeg');   background-repeat: no-repeat; background-size: 200%;"></div>
                         <div class="widget-chart widget-chart-hover">
-                            <div class="widget-numbers">10%</div>
+                            <div class="widget-numbers">-</div>
                             <div class="widget-subheading">Completion Rate</div>
                         </div>
                     </div>
