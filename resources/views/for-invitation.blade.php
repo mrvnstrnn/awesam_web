5c203e661ef6112f8c25b2d659ca317912712b7e
zx016536 <zx016536@globetel.com>
2021-12-26 13:55:58 +0800

    update

--- resources/views/for-invitation.blade.php
+++ resources/views/for-invitation.blade.php
@@ -6,6 +6,7 @@
             <th>Firstname</th>
             <th>Lastname</th>
             <th>Email</th>
+            <th>Programs</th>
             <th>URL</th>
         </tr>
         </thead>
@@ -17,6 +18,24 @@
                     <td>{{ $invitation->firstname }}</td>
                     <td>{{ $invitation->lastname }}</td>
                     <td>{{ $invitation->email }}</td>
+                    <td>
+                        @php
+                            $users = \App\Models\User::select('id')->where('email', $invitation->email)->first();
+                        @endphp
+
+                        @if (is_null($users))
+                            Not yet registered.
+                        @else
+                            @php
+                                $user_programs = \App\Models\UserProgram::join('program', 'program.program_id', 'user_programs.program_id')
+                                                                ->select('program.program')
+                                                                ->where('user_id', $users->id)
+                                                                ->get()
+                                                                ->pluck('program');
+                            @endphp
+                            {{ $user_programs }}
+                        @endif
+                    </td>
                     <td>{{ Request::root() }}/invitation-link/{{ $invitation->token }}/{{ $invitation->invitation_code }}</td>
                 </tr>
             @endforeach
--- resources/views/for-vendor-invitation.blade.php
+++ resources/views/for-vendor-invitation.blade.php
@@ -6,6 +6,7 @@
             <th>Email</th>
             <th>Firstname</th>
             <th>Lastname</th>
+            <th>Programs</th>
             <th>URL</th>
         </tr>
         </thead>
@@ -17,6 +18,24 @@
                     <td>{{ $invitation->vendor_admin_email }}</td>
                     <td>{{ $invitation->vendor_firstname }}</td>
                     <td>{{ $invitation->vendor_lastname }}</td>
+                    <td>
+                        @php
+                            $users = \App\Models\User::select('id')->where('email', $invitation->vendor_admin_email)->first();
+                        @endphp
+
+                        @if (is_null($users))
+                            Not yet registered.
+                        @else
+                            @php
+                                $user_programs = \App\Models\UserProgram::join('program', 'program.program_id', 'user_programs.program_id')
+                                                                ->select('program.program')
+                                                                ->where('user_id', $users->id)
+                                                                ->get()
+                                                                ->pluck('program');
+                            @endphp
+                            {{ $user_programs }}
+                        @endif
+                    </td>
                     <td>{{ route('login') }}</td>
                 </tr>
             @endforeach
