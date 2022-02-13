3011962c847e8c3dfbe05014e32b1784b0f00c99
Marvin Esternon <zx016536@globetel.com>
2022-02-14 02:16:22 +0800

    update

---
+++ resources/views/profiles/globe/director/my-approvals.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.home')
+
+@section('content')
+
+<div class="row">
+    <div class="col-12 text-center">
+        <img src="/images/construction.gif" width="50%"/>
+    </div>
+</div>
+
+@endsection
+
+@section('js_script')
+
+
+<!-- PR PO Counter -->
+{{-- <script type="text/javascript" src="/js/newsites_ajax_counter.js"></script>   --}}
+
+    
+@endsection
